4903243c8ea6942ef81d6a9ef0cdffe8d649f017
Alexander Soudry <soudryalex@gmail.com>
2021-04-21 17:31:59 -0400

    commit

--- src/Event.jsx
+++ src/Event.jsx
@@ -4,11 +4,12 @@ class Event extends Component {
   
    render() {  
      let nope = 0;
-     console.log(nope + 5);
+    //  console.log(nope + 5);
     return <div className="event">
-      <h3 className="summary">{this.props.event.summary}</h3>
-      <h4 className="location" >{this.props.event.location}</h4>
-      <button type="button" className="button">Show Details</button>
+      {/* <h3 className="summary">{this.props.event.summary}</h3>
+      <h4 className="location" >{this.props.event.location}</h4> */}
+      <h1 className="test">{nope}</h1>
+      {/* <button type="button" className="button">Show Details</button> */}
     </div>;
   }
 }
--- src/_tests_/Event.test.js
+++ src/_tests_/Event.test.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Event from '../Event';
-import PropTypes from 'prop-types';
-import { mockData } from '../mockData';
-import { extractTitle, extractLocations } from "../api";
+
+// import { mockData } from '../mockData';
+// import { extractTitle, extractLocations } from "../api";
 
 describe('<Event/> component', () => {
-    let EventWrapper, title, locations
-    beforeAll(() => {
-         console.log(this.props.event);
-         console.log(event);
-         console.log(this.props.nope);
-         console.log(nope);
-        title = extractTitle(mockData),
-        locations = extractLocations(mockData)
-        EventWrapper = shallow(<Event event={mockData0} nope={nope} />)
-    })
-    test('render initial box', () => {
-        expect(EventWrapper.find('.event')).toHaveLength(1);
-    })
-    test('render see more button', () => {
-        expect(EventWrapper.find('.button')).toHaveLength(1);
-    })
+    // let EventWrapper = shallow(<Event />)
+
+//     test("prop", () => {
+//         expect(EventWrapper.find(.event)).props().
+//     })
+
+test('render initial box', () => {
+    let EventWrapper = shallow(<Event />)
+    expect(EventWrapper.find('.event')).toHaveLength(1);
+})
+
+test('prop', () => {
+    let EventWrapper = shallow(<Event prop={prop} />)
+    expect(EventWrapper.find('.test')).prop(nope).toEqual(0);
+})
+
+})
+
+ 
+    // test('render see more button', () => {
+    //     expect(EventWrapper.find('.button')).toHaveLength(1);
+    // })
     // test('render wrapper content 3', () => {
     //     expect(EventWrapper.find('.location').props()).toMatchObject(Location);
     // })
-})
