adc4571e8d7c44b4b7d5e14967f469f0b5cb320c
Alexander Soudry <soudryalex@gmail.com>
2021-06-01 14:42:09 -0400

    changed the function names of updateEvents and passNumber to something to more readable.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import EventNumber from './eventNumber';
-// import { mockData } from './mockData';
 import { getEvents, extractLocations } from './api';
 import "./nprogress.css";
 
@@ -14,8 +13,7 @@ class App extends Component {
     this.state = {
       events: [],
       locations: [],
-      number: 32,
-      storage: []
+      number: 32
     }
   }
 
@@ -24,14 +22,13 @@ componentDidMount() {
   getEvents().then((first) => {
     this.setState({
       events: first,
-      storage: first,
       number: first.length,
       locations: extractLocations(first)
     })
   })
 }
 
-updateEvents = (location) => {
+filterEventsByCity = (location) => {
   getEvents().then((events) => {
     const locationEvents = (location === 'all') ?
     events :
@@ -43,7 +40,7 @@ updateEvents = (location) => {
   })
 }
 
-passNumber = (number) => {
+filterByNumber = (number) => {
   getEvents().then((event) => {
     let filtering = event.length
     if (event.length === number) {
@@ -52,7 +49,6 @@ passNumber = (number) => {
       })
     }
     else if (event.length > number) {
-      console.log(this.state.storage)
       for (number; filtering > number; number++) {
         event.shift()
       } 
@@ -75,9 +71,11 @@ componentWillUnmount() {
 render() {
     return (
       <div className="App">
-         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}  />
-         <EventNumber length={this.state.number} passNumber={this.passNumber}/>
-         <EventList events={this.state.events} filterEvents={this.filterEvents} />
+         <CitySearch locations={this.state.locations} updateEvents={this.filterEventsByCity}  />
+         <EventNumber length={this.state.number} passNumber={this.filterByNumber}/>
+         <EventList events={this.state.events} 
+        //  filterEvents={this.filterEvents} 
+         />
       </div>
     )
   }
