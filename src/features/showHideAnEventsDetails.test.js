78b98570d62440f217e5f02f9c69d83a0b154100
Alexander Soudry <soudryalex@gmail.com>
2021-06-01 14:57:35 -0400

    The tests have been updated to fit the new scenarios. In addition the scenarios have been properly written and functionally render the code needed.

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -10,54 +10,68 @@ const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
 
 defineFeature(feature, test => {
 
-	test('An event element is collapsed by default', ({ given, when, then }) => {
-    	given('the user turns on the application', () => {
+	test('An event element is collapsed by default', ({ given, and, when, then }) => {
+		let EventWrapper
+    	given('the user did not expand any event', () => { });
+		let AppWrapper	
+    	and('the app has mounted', () => {
+			AppWrapper = mount(<App />);
 
     	});
-		let AppWrapper
-    	when('the user firsts opens up the app', () => {
-			AppWrapper = mount(<App />);
+
+    	when('all the events are loaded', () => {
+			AppWrapper.update();
+			expect(AppWrapper.find('.event')).toHaveLength(mockData.length);
     	});
-		let EventWrapper
-    	then('the event element will already be collapsed', () => {
+
+    	then('all the event element will already be collapsed', () => {
 			EventWrapper = mount(<Event event={mockData} />)
 			expect(EventWrapper.state('show')).toBe(false);
-
     	});
     });
 
-    test('User can expand an event to see its details', ({ given, when, then }) => {
-    	given('the user wants to the details of a even', () => {
+    test('User can expand an event to see its details', ({ given, and, when, then }) => {
+		let AppWrapper	
+    	given('the app is loaded', () => {
+			AppWrapper = mount(<App />);
+    	});
+
+    	and('all the events are loaded', () => {
+			AppWrapper.update();
+			expect(AppWrapper.find('.event')).toHaveLength(mockData.length);
 
     	});
 		let EventWrapper
     	when('user clicks on “Show details” button for an event', () => {
 			EventWrapper = mount(<Event event={mockData} />)
 			EventWrapper.find('.button').simulate('click')
+
     	});
 
     	then('the event element will be expanded to show the event details', () => {
-			EventWrapper = mount(<Event event={mockData} />)
-			expect(EventWrapper.state('show')).toBe(false);
+			expect(EventWrapper.state('show')).toBe(true);
     	});
     });
 
-    test('User can collapse an event to hide its details', ({ given, when, then }) => {
-		let EventWrapper
-    	given('the user wants close event element', () => {
-			EventWrapper = mount(<Event event={mockData} />)
-			EventWrapper.setState({show: true})
+    test('User can collapse an event to hide its details', ({ given, and, when, then }) => {
+    	let AppWrapper
+		given('the app is loaded', () => {
+			AppWrapper = mount(<App />);
+    	});
+		let EventWrapper = mount(<Event event={mockData} />)
+    	and('the user expanded event details', () => {
+			EventWrapper.find('.button').simulate('click')
+			expect(EventWrapper.state('show')).toBe(true);
     	});
 
     	when('user clicks on “Close” button for an event', () => {
-			EventWrapper = mount(<Event event={mockData} />)
-			EventWrapper.find('.button').simulate('click')
+			EventWrapper.find('.closing').first().simulate('click')
+			
     	});
 
     	then('the event element will hide its details.', () => {
-			EventWrapper = mount(<Event event={mockData} />)
+			EventWrapper.update
 			expect(EventWrapper.state('show')).toBe(false);
     	});
     });
-
 });
