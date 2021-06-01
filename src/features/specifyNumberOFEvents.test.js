a08a4fb04012f2c7639b42c0cc08b05e1a82d4f9
Alexander Soudry <soudryalex@gmail.com>
2021-06-01 14:58:04 -0400

    The tests have been updated to fit the new scenarios. In addition the scenarios have been properly written and functionally render the code needed.

--- src/features/specifyNumberOFEvents.test.js
+++ src/features/specifyNumberOFEvents.test.js
@@ -2,48 +2,37 @@ import React from 'react';
 import EventNumber from '../eventNumber';
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import { mount } from 'enzyme';
-import { mockData } from '../mockData';
 const feature = loadFeature('./src/features/specifyNumberOFEvents.feature');
+import App from '../App';
+import EventList from '../EventList';
+
 
 defineFeature(feature, test => {
 
-    test('When user hasn’t specified a number, 4 is the default number', ({ given, when, then }) => {
-    	let Numberwrapper
-        given('the user hasn’t specified a number of events.', () => {
-
-    	});
-
-    	when('the user opens up the app.', () => {
-            Numberwrapper = mount(<EventNumber length={mockData.length}/>)
-    	});
-
-    	then(/^the default number that is rendered is (\d+)$/, (arg0) => {
-            Numberwrapper = mount(<EventNumber length={mockData.length}/>)  
-            expect(Numberwrapper.state('number')).toEqual(4)   
-    	});
-    });
-
-    test('User can change the number of events they want to see', ({ given, when, then }) => {
-    	given('the user wants to change the number of events they see', () => {
-
-    	});
-        let Numberwrapper
-    	when('the user changes the input number', () => {
-            let test = 5;
-            Numberwrapper = mount(<EventNumber length={mockData.length} passNumber={() => {
-                    
-            }}/>)
-            Numberwrapper.find('input').simulate('change', {target: {value: 3}});
-    	});
-
-    	then('the number of events displayed will change.', () => {
-            // const passNumber = jest.fn(() => true)
-            // const changeInput = jest.fn(() => {
-            //     Numberwrapper.setState('number').toBe(3)
-            // })
-            // expect(changeInput())
-    	});
-    });
-
- 
+        test('When user hasn\'t specified event number 32 is the default number', ({ given, when, then }) => {
+                given('user hasn\'t specified the number of events', () => { });
+                  let AppWrapper;
+                  when('the user opens the app', () => {
+                    AppWrapper = mount(<App />);
+                  });
+                  then('the maximum of 32 events will be displayed', () => {
+                    AppWrapper.update();
+                    expect((AppWrapper.find('.event')).length).toBeLessThanOrEqual(32);
+                  });
+                });
+    
+        test('User can change the number of events they want to see', ({ given, when, then }) => {
+                let AppWrapper	
+                given('the user opened the app', () => {
+                 AppWrapper = mount(<App />);
+                });
+                when('the user changes the number of events', () => {
+                AppWrapper.find(EventNumber).find('.eventNumberInput').simulate('change', {target: {value: 2}})
+                });
+  
+                then('the number of events displayed will change.', () => {
+                AppWrapper.update()
+                expect((AppWrapper.find(EventList).find('.singleEvent')).length).toEqual(2)
+                });
+        }); 
 })
