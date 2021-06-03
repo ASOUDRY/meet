9403269862a1c0a0d6b4e76f85e3c205735617ce
Alexander Soudry <soudryalex@gmail.com>
2021-06-02 20:50:31 -0400

    Added alert to inform the end user to choose a number between 1 and 32 if they go above 32 or below 0.

--- src/eventNumber.jsx
+++ src/eventNumber.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {ErrorAlert} from './Alert'
 
 class EventNumber extends Component {
   
@@ -14,6 +15,18 @@ class EventNumber extends Component {
 
   changeInput = (input) => {
     let newInput = input.target.value;
+    if (newInput > 32 || newInput < -1) {
+      this.setState({
+        number: newInput,
+        infoText: "Please pick a number between 1 and 32"
+      })
+    }
+    else {
+      this.setState({
+        number: newInput,
+        infoText: ""
+      })
+    } 
     this.setState({
       number: newInput
     })
@@ -25,11 +38,13 @@ class EventNumber extends Component {
     return (
       <div className="numberOfEvents">
         <h1>Number of Events:</h1>
+        <ErrorAlert text={this.state.infoText} />
         <input className="eventNumberInput"
           type="number"
           value={push}
           onChange={this.changeInput}
         />
+       <ErrorAlert className="errorAlert" text={this.state.infoText} />
       </div>
     );
   }
