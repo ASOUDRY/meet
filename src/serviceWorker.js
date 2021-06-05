48177a578b619ab8ad0881484d40f2b93f301d83
Alexander Soudry <soudryalex@gmail.com>
2021-06-04 21:56:35 -0400

    Adding a build to the url to see if that resolves the error.

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -28,7 +28,7 @@ export function register(config) {
       return;
     }
     window.addEventListener("load", () => {
-      const swUrl = `${process.env.PUBLIC_URL}/serviceWorker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/build/serviceWorker.js`;
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
