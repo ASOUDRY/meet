d75b953279d60fc034ac5c77ed1fec641144bebb
Alexander Soudry <soudryalex@gmail.com>
2021-06-09 21:54:36 -0400

    testing the pi chart on a production build.

--- src/EventGenre.jsx
+++ src/EventGenre.jsx
@@ -1,56 +1,49 @@
-import React from 'react'
-// {useEffect, useState} 
+import React, 
+{useEffect, useState} from 'react'
 
 import { PieChart, Pie, ResponsiveContainer } from 'recharts';
 
 
 const EventGenre = ({events}) => {    
     console.log({events})
-    const data = [
-        {
-          "name": "Group A",
-          "value": 400
-        },
-        {
-          "name": "Group B",
-          "value": 300
-        },
-        {
-          "name": "Group C",
-          "value": 300
-        },
-        {
-          "name": "Group D",
-          "value": 200
-        },
-        {
-          "name": "Group E",
-          "value": 200
-        }
-      ];
+    // const data = [
+    //     {
+    //       "name": "Group A",
+    //       "value": 400
+    //     },
+    //     {
+    //       "name": "Group B",
+    //       "value": 300
+    //     },
+    //     {
+    //       "name": "Group C",
+    //       "value": 300
+    //     },
+    //     {
+    //       "name": "Group D",
+    //       "value": 200
+    //     },
+    //     {
+    //       "name": "Group E",
+    //       "value": 200
+    //     }
+    //   ];
 
+      const [data, setData] = useState([]);
 
-
-      // const [data, setData] = useState([]);
-
-      // useEffect(() => {
-      //   setData(() => getData());
-      // }, [events]);
+      useEffect(() => {
+        setData(() => getData());
+      }, [events]);
     
-      // const getData = ({events}) => {
-      //   const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-      //   const data = genres.map((genre) => {
-      //     const value = events.filter(({ summary }) => 
-      //     summary.split(' ').includes(genre)).length;
-      //     return { name: genre, value};
-      //   });
-      // return data;
-      // }
-
-
-
-
-
+      const getData = () => {
+        const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+        const data = genres.map((genre) => {
+          const value = events.filter(({ summary }) => 
+          summary.split(' ').includes(genre)).length;
+          return { name: genre, value};
+        });
+      return data;
+      }
 
     return (
       <ResponsiveContainer height={400}>
